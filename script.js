6353c9598c07444291571179c3f9804f3fcea1b9
GanXx8 <ganda.imasxx@gmail.com>
2021-07-25 20:16:48 +0700

    belajarFunciont01

---
+++ script.js
@@ -0,0 +1,17 @@
+// Belajar funciont sederhana
+
+function jumlahUang(a, b) {
+    var uangJajan;
+    var uangTabungan;
+    var hasil;
+
+    uangJajan = a;
+    uangTabungan= b;
+
+    hasil = uangTabungan + uangJajan;
+
+    return hasil;
+    
+}
+
+console.log(jumlahUang(12000, 15000))
